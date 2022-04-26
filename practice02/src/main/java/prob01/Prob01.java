0f2a5f6d1967315b4312ae98b66c71e8140a0aa5
kwakdo <vv05044@gmail.com>
2022-04-26 16:47:59 +0900

    p1

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
+		Scanner scanner = new Scanner(System.in);
 
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
 		System.out.println("금액: ");
 		int money = 0;
-		int ml[] = new int [MONEYS.length];
+		int ml[] = new int[MONEYS.length];
 		money = scanner.nextInt();
-		for (int i = 0; i<MONEYS.length; i++) {
+		for (int i = 0; i < MONEYS.length; i++) {
 			if (MONEYS[i] <= money) {
-				ml[i] = money/MONEYS[i];
-				money = money%MONEYS[i];
+				ml[i] = money / MONEYS[i];
+				money = money % MONEYS[i];
 			}
 			System.out.println(MONEYS[i] + "원 : " + ml[i] + "개");
 		}
-		
-		
-		
-				
-		
+
 		scanner.close();
- 	}
+	}
 }

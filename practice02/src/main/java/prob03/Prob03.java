026b14edc7e0c8a1c79a908c9bf76bde30ee5ecb
kwakdo <vv05044@gmail.com>
2022-04-26 18:01:07 +0900

    p3

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -5,13 +5,18 @@ public class Prob03 {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
 		// 원래 배열 원소 출력
-		printCharArray(c);
+		//printCharArray(c);
+		String str = new String(c); 
 
 		// 공백 문자 바꾸기
-		replaceSpace(c);
+		//replaceSpace(c);
+		String text = str.replace(' ', ',');
+
 
 		// 수정된 배열 원소 출력
-		printCharArray(c);
+		//printCharArray(c);
+		System.out.println(str);
+		System.out.println(text);
 	}
 
 }

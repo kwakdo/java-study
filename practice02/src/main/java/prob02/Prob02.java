cc1deaa7c87f392080c800485e85d5d18d2720de
kwakdo <vv05044@gmail.com>
2022-04-26 17:22:31 +0900

    prob2

--- practice02/src/main/java/prob02/Prob02.java
+++ practice02/src/main/java/prob02/Prob02.java
@@ -10,12 +10,20 @@ public class Prob02 {
 		int[] intArray = new int[ 5 ];
 		double sum = 0;
 
-		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
+		System.out.println("5개의 숫자를 입력하세요.");
+		intArray[0] = scanner.nextInt();
+		intArray[1] = scanner.nextInt();
+		intArray[2] = scanner.nextInt();
+		intArray[3] = scanner.nextInt();
+		intArray[4] = scanner.nextInt();
+		
 		
 		/* 배열에 저장된 정수 값 더하기 */
+		sum = intArray[0] + intArray[1] + intArray[2] + intArray[3] + intArray[4];
+		double av = sum/5;
 		
 		/* 출력 */
-		
+		System.out.println("평균은 " + av + " 입니다.");
 		/* 자원정리 */
 		scanner.close();
 	}

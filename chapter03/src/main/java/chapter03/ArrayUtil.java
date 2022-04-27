c860c022b445426e2ca1b8ae970a7b03edf56864
kwakdo <vv05044@gmail.com>
2022-04-27 17:21:57 +0900

    Array Util

---
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -0,0 +1,23 @@
+package chapter03;
+
+public class ArrayUtil {
+
+	public static double[] intToDouble(int[] a) {
+		double[] result = new double[a.length];
+		for(int i = 0; i < a.length; i++) {
+			result[i] = a[i];
+		}
+		return result;
+	}
+
+	public static int[] DoubleToInt(double[] ds) {
+		
+		return null;
+	}
+
+	public static int[] concat(int[] is, int[] is2) {
+		
+		return null;
+	}
+
+}

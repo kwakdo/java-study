4d6e751513ba18c586c77169d0e9fd06f2b5c67a
kwakdo <vv05044@gmail.com>
2022-04-27 11:18:12 +0900

    접근 제어 키워드

--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -4,6 +4,11 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods goods = new Goods();
+		
+		goods.setName("nikon");
+		goods.setPrice(400000);
+		goods.setCountSold(50);
+		goods.setCountStock (30);
+		
+		}
 	}
-
-}

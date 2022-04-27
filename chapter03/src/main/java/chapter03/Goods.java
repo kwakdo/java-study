0fbfcbb46fd302bb3fc1574c91b775db5b9ff047
kwakdo <vv05044@gmail.com>
2022-04-27 10:07:08 +0900

    0427 10:00

---
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -0,0 +1,8 @@
+package chapter03;
+
+public class Goods {
+	int price;
+	int countStock;
+	int countSold;
+
+}
---
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -0,0 +1,9 @@
+package chapter03;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods goods = new Goods();
+	}
+
+}

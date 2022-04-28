3cd989eebe812afa25b64564e62e8b16e7626049
kwakdo <vv05044@gmail.com>
2022-04-28 17:59:23 +0900

    0428 18:00

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -7,16 +7,44 @@ public class Main {
 		point1.setX(20);
 		point1.setY(10);
 
-		//point1.show();
+		// point1.show();
 		drawPoint(point1);
 
 		Point point2 = new Point(50, 100);
 		drawPoint(point2);
 		point2.show(false);
-	}
+
+		Point point3 = new ColorPoint(40, 50, "red");
+		drawPoint(point3);
+		// point3.show(true);
+		
+		Triangle triangle = new Triangle();
+		drawShape(triangle);
 	
+		Rectangle rectangle = new Rectangle();
+		drawShape(rectangle);
+		
+		Circle circle = new Circle();
+		drawShape(circle);
+		}
+	private static void drawTriangle(Triangle triangle) {
+		
+	}
 
 	public static void drawPoint(Point point) {
 		point.show();
+	}
+	public static void drawShape(Shape shape) {
+		shape.draw();
+	}
+	// Triangle triangle = new drawPoint(Point point) {
+	//	point.show();
+	// }
+	// private static void drawColorPoint(ColorPoint point) {
+	// point.show();
+
+	// }
+	public static void drawRectangle(Rectangle rectangle){
+		
 	}
 }

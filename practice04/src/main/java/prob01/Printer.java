27784eb1dd30f20f1e71d693fb4deb0c06e97b38
Kwakdo <vv05044@gmail.com>
2022-05-02 21:57:14 +0900

    prctice04 update

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,7 +2,22 @@ package prob01;
 
 public class Printer {
 
-		
-	}
-	private 
+   public <T> void println(T t) {
+      System.out.println(t);
+   }
+
+   public int sum(Integer... nums) {
+      int sum = 0;
+      for (Integer i : nums) {
+         sum += i;
+      }
+      return sum;
+   }
+
+   public <T> void println(T... ts) {
+      for (T t : ts) {
+         System.out.println(t);
+      }
+   }
+
 }
--- practice04/src/main/java/prob05/MyBase.java
+++ practice04/src/main/java/prob05/MyBase.java
@@ -2,4 +2,18 @@ package prob05;
 
 public class MyBase extends Base {
 
+	@Override
+	public void service(String time) {
+	
+		if (time.equals("오후")) {
+			System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+
+		} else if (time.equals("낮")) {
+			System.out.println("낮에는 열심히 일하자!");
+
+		} else {
+			night();
+		}
+	}
+
 }

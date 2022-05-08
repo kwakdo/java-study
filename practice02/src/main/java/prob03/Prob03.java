c90ac40449af7ef1b0704b1b4ec80ace1d86d204
kwakdo <104194344+kwakdo@users.noreply.github.com>
2022-05-08 17:24:21 +0900

    Update Prob03.java

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -5,18 +5,29 @@ public class Prob03 {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
 		// 원래 배열 원소 출력
-		//printCharArray(c);
-		String str = new String(c); 
+		printCharArray(c);
+		// String str = new String(c);
 
 		// 공백 문자 바꾸기
-		//replaceSpace(c);
-		String text = str.replace(' ', ',');
-
+		replaceSpace(c);
+		// String text = str.replace(' ', ',');
 
 		// 수정된 배열 원소 출력
-		//printCharArray(c);
-		System.out.println(str);
-		System.out.println(text);
+		printCharArray(c);
 	}
+		
+		// System.out.println(str);
+		// System.out.println(text);
+		public static void replaceSpace(char c[]) {
+			for(int i = 0; i < c.length; i++) {
+				if(c[i] == ' ') {
+					c[i] = ',';
+				}
+			}
+		}
 
-}
+		public static void printCharArray(char[] c) {
+			System.out.println(c);
+		}
+
+	}

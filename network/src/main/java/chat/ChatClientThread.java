ad8150333f0051e3515f81641461113049f48df3
kwakdo <104194344+kwakdo@users.noreply.github.com>
2022-05-10 00:10:14 +0900

    Update ChatClientThread.java

--- network/src/main/java/chat/ChatClientThread.java
+++ network/src/main/java/chat/ChatClientThread.java
@@ -3,53 +3,36 @@ package chat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
 
 public class ChatClientThread extends Thread {
-	
+
 	private Socket socket;
+	private BufferedReader bufferedReader;
 
 	public ChatClientThread(Socket socket, BufferedReader bufferedReader) {
-		
+		this.socket = socket;
+		this.bufferedReader = bufferedReader;
+
 	}
 
 	@Override
 	public void run() {
-		InetSocketAddress inetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
-		String remoteHostAddress = inetSocketAddress.getAddress().getHostAddress();
-		int remoteHostPort = inetSocketAddress.getPort();
-		ChatServer.log("connected by client[" + remoteHostAddress + ":" + remoteHostPort + "]");
-		
+
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+			while (true) {
+				String receive = bufferedReader.readLine();
+				System.out.println(receive);
+			}
+
+		} catch (SocketException ex) {
+			System.out.println("Chatting Ended.");
 			
-			while(true) {
-				String data = br.readLine();
-				if(data == null) {
-					ChatServer.log("closed by client");
-					break;
-				}
-				
-				ChatServer.log("received:" + data);
-				pw.println(data);
-			}
-		} catch(SocketException ex) {
-			System.out.println("[server] suddenly closed by client");
-		} catch(IOException ex) {
-			System.out.println("[server] error:" + ex);
-		} finally {
-			try {
-				if(socket != null && !socket.isClosed()) {
-					socket.close();
-				}
-			} catch(IOException ex) {
-				ex.printStackTrace();
-			}
+		} catch (IOException e) {
+			System.out.println("error: " + e);
 		}
 	}
 }

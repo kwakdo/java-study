436199fe353dbdbf4c5ec973860a7c55b376063a
kwakdo <104194344+kwakdo@users.noreply.github.com>
2022-05-15 00:47:12 +0900

    Update ChatClientApp.java

--- network/src/main/java/chat/gui/ChatClientApp.java
+++ network/src/main/java/chat/gui/ChatClientApp.java
@@ -33,56 +33,34 @@ public class ChatClientApp {
 			System.out.println("대화명은 한 글자 이상 입력해야 합니다.\n");
 		}
 		
-		//scanner.close();
+	scanner.close();
 		
 		try {
 
-		// 1. create socket
 		socket = new Socket();
 		
-		// 2. connect server
 		socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
 		log("Connected.");
 		
-		// 3. get iostream(pipline established)
 		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 		PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
 		
-		// 4. join protocol 처리
-		//System.out.print("닉네임>>");
-		//String nickname = scanner.nextLine();
-			
-		//    pw.println("join:둘리"); 
 		pw.println("join:" + name);
-		
-		//    String line = br.readLine();
-		String line = br.readLine();
-		
-		// String line = "JOIN:OK";
-		
-		if("JOIN:OK".equals(line)) {
-			new ChatWindow(name).show();
-		}
-			
-		} catch (IOException e) {
-			log("error: " + e);
-			
-		} finally {
-			
-			try {
-				if (scanner != null) {
-					scanner.close();
-				}
-				if (socket != null && socket.isClosed() == false) {
-					socket.close();
-				}
-			} catch (IOException ex) {
-				log("error:" + ex);
+		pw.flush();
 
-			}
+		String line = br.readLine();
+		String[] tokens = line.split(":");
+		if ("join".equals(tokens[0])) {
+			new ChatWindow(name, socket, br, pw).show();
+		} else {
+			return;
 		}
-		}
-	private static void log(String log) {
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+
+}
+	public static void log(String log) {
 		System.out.println("[client] " + log);
 	}
 }
